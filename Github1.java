8e490da958d7a5edcf85dbaa45f64f0a4cb4f87c
nadhiefa286 <nadhiefaputri20@gmail.com>
2025-02-14 09:59:29 +0700

    Create Github1.java

---
+++ Github1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.github1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Github1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello github1");
+    }
+}
